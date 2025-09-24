739290b7b6d856f3f5b380f0c190f25d08c85541
Kennedy (whitepc) <kennedy.lopes@ufersa.edu.br>
2025-09-24 15:04:05 -0300

    Fibonacci

--- aula02/aula02.c
+++ aula02/aula02.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>//atoi
+#include <math.h>
 
-
-int main(){
-    printf("Hello, World!\n");
-    return 0;
+int fib(int);
+int main(int argc, char *argv[]){
+    int res = fib(atoi(argv[1]));
+    printf("Fib(%d) = %d\n", atoi(argv[1]), res);
 }
+int fib(int x){
+    if(x == 0 || x == 1){
+        return 1;
+    }
+    return fib(x-1) + fib(x-2);
+}
+// clock_t inicio, fim;
+
+// inicio = clock();//Start
+// for (size_t i = 0; i < 10000000; i++);
+// fim = clock();//End
+
+// double d = ((double)(fim - inicio))/CLOCKS_PER_SEC;
+
+// printf("Tempo: %f\n", d);
